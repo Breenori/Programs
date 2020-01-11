cdae58b155176c0da981ba7bc744a95a29a5823a
Breetzi <bastianbreetz@gmail.com>
2020-01-11 16:32:51 +0100

    Finish UE8

--- ADS_Pritz_UE8/ADS_Pritz_UE8/Alternative.cpp
+++ ADS_Pritz_UE8/ADS_Pritz_UE8/Alternative.cpp
@@ -2,97 +2,139 @@
 #include<string>
 #include<fstream>
 #include<vector>
-#include<time.h>
+#include<random>
+#include<iomanip>
+#include <sstream>
 using namespace std;
 
-enum role_type { nitwit, propagator, suppressor };
+enum role_type { rt_nitwit, rt_propagator, rt_suppressor };
+enum method_type { mt_daley, mt_maki, mt_maki_linear};
+
+// Returns the role of a random person (with their index) based on the number of the roles.
+role_type get_random_role(int const index, int const nitwits, int const propagators, int const suppressors);
+// Runs a simulation of rumour spreading with the defined method and population size and logs the values to a file.
+void rumour_simulation(int const size, method_type method, string const& outputfile);
+// Reads and returns a number and makes sure it's valid
+int read_number();
 
 void main()
 {
-	int n = 100000;
+	cout << "Please enter the population size: ";
+	int population_size(read_number());
+	if (population_size > 10000)
+	{
+		cout << "Please wait! Based on the entered number this can take a while...\n";
+	}
+	rumour_simulation(population_size, mt_maki, "outputti.csv");
+}
 
-	srand((int)time(0));
+void rumour_simulation(int const size, method_type const method, string const& outputfile)
+{
+	// Normal rand() function can only get numbers up to about 32000.
+	// Thats why a different random number function is needed.
+	random_device rand_dev;
+	mt19937 rand_gen(rand_dev());
+	uniform_real_distribution<> rand_distrib(0, size);
 
+	// At the beginning there is only 1 propagator, the rest is made up of nitwits.
 	int propagators(1);
-	int nitwits(n - 1);
+	int nitwits(size - 1);
 	int suppressors(0);
 
-	ofstream out("outputi.csv");
-	out.imbue(locale("de"));
+	// The files should be written to a file that can be opened in excel.
+	ofstream out(outputfile);
 
-	string text("");
-	text += "\"nitwits\";\"propagators\";\"suppressors\"\n";
+	out << "\"nitwits\";\"propagators\";\"suppressors\"\n";
+	out << to_string(nitwits) + ";" + to_string(propagators) + ";" + to_string(suppressors) + "\n";
 	while (propagators > 0 && nitwits > 0)
 	{
-		int p1_index = rand() % (propagators + nitwits + suppressors);
-		int p2_index = rand() % (propagators + nitwits + suppressors);
+		// First, we need 2 random people.
+		int p1_index = rand_distrib(rand_gen);
+		int p2_index = rand_distrib(rand_gen);
 
+		// If the same person has been selected twice, make sure they are different afterwards.
 		while (p2_index == p1_index)
 		{
-			p2_index = rand() % n;
+			p2_index = rand_distrib(rand_gen);
 		}
 
-		role_type person1;
-		if (p1_index < nitwits)
+		// Since this is the approach without a vector, the role has to be determined using the index and the distribution of roles.
+		role_type person1(get_random_role(p1_index, nitwits, propagators, suppressors));
+		role_type person2(get_random_role(p2_index, nitwits, propagators, suppressors));
+
+		if (method == mt_maki_linear)
 		{
-			person1 = nitwit;
-		}
-		else if (p1_index < (nitwits + propagators))
-		{
-			person1 = propagator;
-		}
-		else
-		{
-			person1 = suppressor;
+			if (propagators > 1 || (person2 != rt_propagator && propagators == 1))
+			{
+				person1 = rt_propagator;
+			}
 		}
 
-		role_type person2;
-		if (p2_index < nitwits)
-		{
-			person2 = nitwit;
-		}
-		else if (p2_index < (nitwits + propagators))
-		{
-			person2 = propagator;
-		}
-		else
-		{
-			person2 = suppressor;
-		}
-
-		if (person1 == propagator && person2 == nitwit || person1 == nitwit && person2 == propagator)
+		if (person1 == rt_propagator && person2 == rt_nitwit || person1 == rt_nitwit && person2 == rt_propagator)
 		{
 			propagators++;
 			nitwits--;
 		}
-		else if (person1 == propagator && person2 == propagator)
+		else if (person1 == rt_propagator && person2 == rt_propagator)
 		{
-			suppressors += 2;
-			propagators -= 2;
+			// Only one propagator becomes a suppressor for maki, but both become one for kendal method.
+			suppressors += 1;
+			propagators -= 1;
+			if (method == mt_daley)
+			{
+				suppressors += 1;
+				propagators -= 1;
+			}
 		}
-		else if (person1 == propagator && person2 == suppressor || person1 == suppressor && person2 == propagator)
+		else if (person1 == rt_propagator && person2 == rt_suppressor || person1 == rt_suppressor && person2 == rt_propagator)
 		{
 			propagators--;
 			suppressors++;
 		}
 
-		//out << nitwits << ";" << propagators << ";" << suppressors << std::endl;
-		text += nitwits;
-		text += ";";
-		text += propagators;
-		text += ";";
-		text += suppressors;
-		text += "\n";
-
-		if (text.length() > 10000)
-		{
-			out << text;
-			text.clear();
-		}
-		//cout << (nitwits * 1.0) / n << ";" << (propagators * 1.0) / n << ";" << (suppressors * 1.0) / n << std::endl;
+		// Write the current state to the specified file
+		out << to_string(nitwits) + ";" + to_string(propagators) + ";" + to_string(suppressors) + "\n";
 	}
-	out << text;
 	out.close();
 
-	cout << (nitwits * 1.0) / n << ";" << (propagators * 1.0) / n << ";" << (suppressors * 1.0) / n << std::endl;
+	// Write the summary to the console with 1 number after the comma.
+	// Role-name length has been accounted for in the width, so the values are in line.
+	cout << "nitwits: " << setw(20 - 7) << fixed << setprecision(1) << nitwits * 100.0 / size << "%\n";
+	cout << "propagators: " << setw(20 - 11) << fixed << setprecision(1) << propagators * 100.0 / size << "%\n";
+	cout << "suppressors: " << setw(20 - 11) << fixed << setprecision(1) << suppressors * 100.0 / size << "%";
+}
+role_type get_random_role(int const index, int const nitwits, int const propagators, int const suppressors)
+{
+	// Taking the index and the number of certain roles into account, a random role can be determined without a vector.
+	if (index < nitwits)
+	{
+		return rt_nitwit;
+	}
+	else if (index < (nitwits + propagators))
+	{
+		return rt_propagator;
+	}
+	else
+	{
+		return rt_suppressor;
+	}
+}
+int read_number()
+{
+	int num(0);
+	cin >> num;
+
+	// make sure the user enters a valid number
+	while (cin.fail())
+	{
+		num = 0;
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cout << "The value must be numeric (MAX:" << INT_MAX << "): ";
+		cin >> num;
+	}
+	// If there's something after the number, clear it.
+	cin.clear();
+	cin.ignore(INT_MAX, '\n');
+	return num;
 }
--- ADS_Pritz_UE8/ADS_Pritz_UE8/Source.cpp
+++ ADS_Pritz_UE8/ADS_Pritz_UE8/Source.cpp
@@ -1,83 +1,143 @@
-
 /*#include<iostream>
 #include<string>
 #include<fstream>
 #include<vector>
-#include<time.h>
+#include<random>
+#include<iomanip>
 using namespace std;
 
-enum role_type {nitwit, propagator, suppressor};
+enum role_type { rt_nitwit, rt_propagator, rt_suppressor};
+enum method_type { mt_daley, mt_maki, mt_maki_linear};
+
+// Reads and returns a number and makes sure it's valid
+int read_number();
+// Runs a simulation of rumour spreading with the defined method and population size and logs the values to a file.
+void rumour_simulation(vector<role_type>& population, int const size, method_type const method, string const& outputfile);
 
 void main()
 {
-	int n = 1000000;
-	vector<role_type> population(n);
-	
-	for (size_t i(0); i < n; i++)
+	cout << "Please enter the population size: ";
+	int population_size(read_number());
+
+	// Create the vector: Vector has standard value of nitwit (because nitwit = 0) and therefore doesn't have to be initialized.
+	vector<role_type> population(population_size);
+	// We set the first person in the vector to propagator. It literally doesn't matter where we place it.
+	population[0] = rt_propagator;
+
+	if (population_size > 10000)
 	{
-		population[i] = nitwit;
+		cout << "Please wait! Based on the entered number this can take a while...\n";
 	}
+	rumour_simulation(population, population_size, mt_daley, "output.csv");
+}
 
+void rumour_simulation(vector<role_type>& population, int const size, method_type const method, string const& outputfile)
+{
+	// Normal rand function can only return numbers up to about 32000.
+	// That's why another random function is needed.
+	random_device rand_dev;
+	mt19937 rand_gen(rand_dev());
+	uniform_real_distribution<> rand_distrib(0, size);
 
-	srand((int)time(0));
-	population[rand() % n] = propagator;
-
+	// We start with one propagator amidst nitwits.
 	int propagators(1);
-	int nitwits(n - 1);
+	int nitwits(size - 1);
 	int suppressors(0);
 
-	ofstream out("output1.csv");
-	out.imbue(locale("de"));
+	ofstream out(outputfile);
 	out << "\"nitwits\";\"propagators\";\"suppressors\"\n";
-	while(nitwits > 0 && propagators > 0)
+
+	while (nitwits > 0 && propagators > 0)
 	{
-		int p1_index = rand() % n;
-		int p2_index = rand() % n;
-
-		while (p2_index == p1_index)
+		// Choose the first person. For maki_linear method, this will always be a propagator
+		int p1_index(rand_distrib(rand_gen));
+		if (method == mt_maki_linear)
 		{
-			p2_index = rand() % n;
+			if (propagators >= 1)
+			{
+				int index(0);
+				while (population[p1_index] != rt_propagator)
+				{
+					p1_index = index;
+					index++;
+				}
+			}
 		}
-		
 
-		if (population[p1_index] == propagator && population[p2_index] == nitwit)
+		// Choose the 2nd person, this cannot be the same as the first.
+		int p2_index(rand_distrib(rand_gen));
+		while (p1_index == p2_index)
 		{
-			population[p2_index] = propagator;
+			p2_index = rand_distrib(rand_gen);
+		}
+
+		if (population[p1_index] == rt_propagator && population[p2_index] == rt_nitwit)
+		{
+			population[p2_index] = rt_propagator;
 			propagators++;
 			nitwits--;
 		}
-		else if (population[p2_index] == propagator && population[p1_index] == nitwit)
+		else if (population[p1_index] == rt_nitwit && population[p2_index] == rt_propagator)
 		{
-			population[p1_index] = propagator;
+			population[p1_index] = rt_propagator;
 			propagators++;
 			nitwits--;
 		}
-		else if (population[p1_index] == propagator && population[p2_index] == propagator)
+		// If both persons are propagators:
+		// Daley: Both become suppressors
+		// Maki: Only one becomes a suppressor
+		else if (population[p1_index] == rt_propagator && population[p2_index] == rt_propagator)
 		{
-			population[p1_index] = suppressor;
-			population[p2_index] = suppressor;
-			suppressors+=2;
-			propagators-=2;
-			//suppressors += 1;
-			//propagators -= 1;
+			population[p1_index] = rt_suppressor;
+			suppressors += 1;
+			propagators -= 1;
+
+			if (method == mt_daley)
+			{
+				population[p2_index] = rt_suppressor;
+				suppressors += 1;
+				propagators -= 1;
+			}
 		}
-		else if (population[p1_index] == propagator && population[p2_index] == suppressor)
+		else if (population[p1_index] == rt_propagator && population[p2_index] == rt_suppressor)
 		{
-			population[p1_index] = suppressor;
+			population[p1_index] = rt_suppressor;
 			propagators--;
 			suppressors++;
 		}
-		else if (population[p2_index] == propagator && population[p1_index] == suppressor)
+		else if (population[p1_index] == rt_suppressor && population[p2_index] == rt_propagator)
 		{
-			population[p2_index] = suppressor;
+			population[p2_index] = rt_suppressor;
 			suppressors++;
 			propagators--;
 		}
 
-		out << nitwits << ";" << propagators << ";" << suppressors << std::endl;
+		out << to_string(nitwits) + ";" + to_string(propagators) + ";" + to_string(suppressors) + "\n";
 	}
 	out.close();
 
-	cout << (nitwits*1.0)/n << ";" << (propagators*1.0)/n << ";" << (suppressors*1.0)/n << std::endl;
+	// Print the endresult to the console.
+	cout << "nitwits: " << setw(20-7) << fixed << setprecision(1) << nitwits * 100.0 / size << "%\n";
+	cout << "propagators: " << setw(20 - 11) << fixed << setprecision(1) << propagators * 100.0 / size << "%\n";
+	cout << "suppressors: " << setw(20 - 11) << fixed << setprecision(1) << suppressors * 100.0 / size << "%";
+}
+int read_number()
+{
+	int num(0);
+	cin >> num;
+
+	// make sure the user enters a valid number
+	while (cin.fail())
+	{
+		num = 0;
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cout << "The value must be numeric (MAX:" << INT_MAX << "): ";
+		cin >> num;
+	}
+	// If there's something after the number, clear it.
+	cin.clear();
+	cin.ignore(INT_MAX, '\n');
+	return num;
 }
 */
